41d25b0a9b6bfb07239d8c540cbd2e1094c9cb11
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-12-16 14:42:19 -0800

    add more unit tests for user registration

--- accounts/tests/test_accounts_api.py
+++ accounts/tests/test_accounts_api.py
@@ -25,3 +25,43 @@ class PublicAuthApiTests(APITestCase):
         user = User.objects.get(email=payload['email'])
         self.assertEqual(user.name, payload['name'])
         self.assertTrue(user.check_password(payload["password"]))
+
+    def test_register_creates_user_missing_name(self):
+        payload = {
+            "name": "",
+            "email": "testing123@example.com",
+            "password": "testing123",
+            "password_confirm": "testing123",
+        }
+        res = self.client.post(REGISTER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_register_creates_user_missing_email(self):
+        payload = {
+            "name": "Test",
+            "email": "",
+            "password": "testing123",
+            "password_confirm": "testing123",
+        }
+        res = self.client.post(REGISTER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_register_creates_user_missing_password(self):
+        payload = {
+            "name": "Test",
+            "email": "testing123@example.com",
+            "password": "",
+            "password_confirm": "testing123",
+        }
+        res = self.client.post(REGISTER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_register_creates_user_missing_password_confirm(self):
+        payload = {
+            "name": "Test",
+            "email": "testing123@example.com",
+            "password": "testing123",
+            "password_confirm": "",
+        }
+        res = self.client.post(REGISTER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
