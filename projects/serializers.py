f00753f38ef49ef03b2421b2df86ef5f7d662940
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-12-22 11:43:18 -0800

    finish unit tests for basic crud operations with respect to projects

--- projects/serializers.py
+++ projects/serializers.py
@@ -5,5 +5,5 @@ from rest_framework import serializers
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ["name", "description", "created_at", "updated_at"]
+        fields = ["id", "name", "description", "created_at", "updated_at"]
         read_only_fields = ["id", "created_at", "updated_at"]
--- projects/tests/test_projects_api.py
+++ projects/tests/test_projects_api.py
@@ -6,9 +6,16 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from projects.models import (
+    Project,
+)
 
 User = get_user_model()
-CREATE_PROJECT_URL = reverse("projects:projects-list")
+PROJECT_URL = reverse("projects:projects-list")
+
+
+def detail_url(project_id):
+    return reverse("projects:projects-detail", args=[project_id])
 
 
 def create_user(**params):
@@ -27,6 +34,16 @@ def create_user(**params):
     return user
 
 
+def create_project(user, **params):
+    defaults = {
+        "name": "TestDefault",
+        "description": "Default",
+    }
+    defaults.update(params)
+    project = Project.objects.create(user=user, **defaults)
+    return project
+
+
 class PrivateAuthApiTests(APITestCase):
     def setUp(self):
         self.password = "testpass123"
@@ -39,5 +56,44 @@ class PrivateAuthApiTests(APITestCase):
             "name": "Test",
             "description": "testing123testing123testing123testing123testing123",
         }
-        res = self.client.post(CREATE_PROJECT_URL, payload)
+        res = self.client.post(PROJECT_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_update_project_partial(self):
+        project = create_project(
+            user=self.user,
+        )
+
+        payload = {"description": "testingUpdated"}
+        url = detail_url(project.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        project.refresh_from_db()
+        self.assertEqual(project.name, "TestDefault")
+        self.assertEqual(project.description, "testingUpdated")
+
+    def test_update_project_full(self):
+        project = create_project(
+            user=self.user,
+        )
+
+        payload = {
+            "name": "Test Full Update",
+            "description": "New project description",
+        }
+        url = detail_url(project.id)
+        res = self.client.put(url, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        project.refresh_from_db()
+        for key, value in payload.items():
+            self.assertEqual(getattr(project, key), value)
+
+    def test_delete_project(self):
+        project = create_project(
+            user=self.user,
+        )
+        url = detail_url(project.id)
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Project.objects.filter(id=project.id).exists())
